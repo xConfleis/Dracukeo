31c808622bb5699ced1db2b4c4624fd92b1c41c2
xConfleis <bassdubplate@gmail.com>
2019-09-23 18:22:19 -0500

    nice
    to bien

---
+++ holaMundo/src/main/java/com/tecmm/App.java
@@ -0,0 +1,13 @@
+package com.tecmm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo" );
+    }
+}
